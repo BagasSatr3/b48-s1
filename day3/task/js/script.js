30c914bc83de4b711157b0e60053f63d2cb07ff7
BagasSatr3 <103878193+BagasSatr3@users.noreply.github.com>
2023-07-02 08:36:59 +0700

    up

--- day3/task/js/script.js
+++ day3/task/js/script.js
@@ -31,6 +31,18 @@ function mail() {
     let subject = document.getElementById("subject").value
     let message = document.getElementById("message").value
 
+    if(name == "") {
+        return alert("Please insert your Name")
+    } else if(email == "") {
+        return alert("Please insert your Email address")
+    } else if(number == "") {
+        return alert("Insert your phone number, please")
+    } else if(subject == "") {
+        return alert("Select your subject, please")
+    } else if(message == ""){
+        return alert("Typing your message, please")
+    }
+
     const destination = "bagassatjin23@gmail.com"
     let a = document.createElement("a")
     a.setAttribute('href', 'mailto:${destination}?subject=${subject}&body= Hello, my name is ${name} , my reason contact you is ${message}, contact me at ${number}')
